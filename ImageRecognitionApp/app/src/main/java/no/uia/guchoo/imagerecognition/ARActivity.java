ca88a1534ebac51fac87598b63e0f47a65790518
Guro <orug_oedesnel@hotmail.com>
2015-03-26 18:46:48 +0100

    Working model

---
+++ ImageRecognitionApp/app/src/main/java/no/uia/guchoo/imagerecognition/ARActivity.java
@@ -0,0 +1,50 @@
+package no.uia.guchoo.imagerecognition;
+
+
+import android.content.ContentProvider;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.metaio.sdk.ARViewActivity;
+import com.metaio.sdk.jni.IGeometry;
+import com.metaio.sdk.jni.IMetaioSDKCallback;
+
+
+/**
+ * Created by Guro on 26.03.2015.
+ */
+public class ARActivity extends ARViewActivity {
+
+    @Override
+    protected int getGUILayout() {
+        return R.layout.ar_view;
+    }
+
+    @Override
+    protected IMetaioSDKCallback getMetaioSDKCallbackHandler() {
+        return null;
+    }
+
+    @Override
+    protected void loadContents() {
+
+    }
+
+    @Override
+    protected void onGeometryTouched(IGeometry geometry) {
+
+    }
+
+    public void onButtonClick(View v){
+        TextView message = (TextView) findViewById(R.id.messageLabel);
+        message.setText("Heisann!");
+        if(message.getVisibility() == View.INVISIBLE){
+            message.setVisibility(View.VISIBLE);
+        }
+        else{
+            message.setVisibility(View.INVISIBLE);
+        }
+    }
+}
+
