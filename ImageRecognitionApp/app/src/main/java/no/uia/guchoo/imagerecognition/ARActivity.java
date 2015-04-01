20131f10d4bab53dc901ada7649a57975cf49f24
Guro <orug_oedesnel@hotmail.com>
2015-04-01 13:41:22 +0200

    Images saved on disk. Pics every sec.

--- ImageRecognitionApp/app/src/main/java/no/uia/guchoo/imagerecognition/ARActivity.java
+++ ImageRecognitionApp/app/src/main/java/no/uia/guchoo/imagerecognition/ARActivity.java
@@ -3,6 +3,7 @@ package no.uia.guchoo.imagerecognition;
 import android.content.ContentValues;
 import android.os.Environment;
 import android.provider.MediaStore;
+import android.util.Log;
 import android.view.Gravity;
 import android.view.View;
 import android.widget.Toast;
@@ -16,12 +17,15 @@ import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.util.Random;
+import java.util.Timer;
+import java.util.TimerTask;
 
 
 /**
  * Created by Guro on 26.03.2015.
  */
 public class ARActivity extends ARViewActivity {
+ Timer timer;
 
     @Override
     protected int getGUILayout() {return R.layout.ar_view;
@@ -34,12 +38,44 @@ public class ARActivity extends ARViewActivity {
 
     @Override
     protected void loadContents() {
+        timer = new Timer();
+        timer.scheduleAtFixedRate(new TimerTask() {
+            @Override
+            public void run() {
+                takeImage();
+            }
+        }, 0, 1000);
 
     }
 
     @Override
     protected void onGeometryTouched(IGeometry geometry) {
 
+    }
+
+    public void takeImage() {
+        Log.d("image", "taking image");
+            String dir = Environment.getExternalStorageDirectory().toString();
+            File rootDir = new File(dir + File.separator + "album");
+                if (!rootDir.exists()) {
+                    rootDir.mkdir();
+                }
+                Random generator = new Random();
+                int i = 10000;
+        i = generator.nextInt(i);
+                String fname = "GG-" + i + ".jpg";
+                File file = new File(rootDir, fname);
+                if (file.exists()) file.delete();
+                try {
+                    FileOutputStream out = new FileOutputStream(file);
+                    out.flush();
+                    out.close();
+                } catch (FileNotFoundException et) {
+                    et.printStackTrace();
+                } catch (IOException ex) {
+                    ex.printStackTrace();
+                }
+                addImageGallery(file);
     }
 
     public void onButtonClick(View v){
@@ -65,14 +101,18 @@ public class ARActivity extends ARViewActivity {
         } catch (IOException e) {
             e.printStackTrace();
         }
-        
+
+        showMessage(fname);
+        addImageGallery(file);
+    }
+
+    private void showMessage(String fname){
         String imagepath = Environment.getExternalStorageDirectory().getPath() + File.separator + "album" + File.separator + fname;
         metaioSDK.requestScreenshot(imagepath);
         String message = "Image Saved";
         Toast toast = Toast.makeText(getApplicationContext(),message,Toast.LENGTH_SHORT);
         toast.setGravity(Gravity.CENTER,0,0);
         toast.show();
-        addImageGallery(file);
     }
 
     private void addImageGallery(File file) {
